e1a7084b03d6359c5c953b19741ed68347807990
Adrian Salamanca Dolores <salamancadoloresadrian@gmail.com>
2023-09-20 12:19:13 -0600

    12 retos JS con iteradores

---
+++ reto3.js
@@ -0,0 +1,19 @@
+function distributeGifts(packOfGifts, reindeers) {
+    let totalGifts = 0;
+    let totalRenos = 0;
+    packOfGifts.fotEach(
+        gift => totalGifts += gift.length
+    );
+    reindeers.forEach(
+        reinder => totalRenos = reinder.length *2
+    );
+    return Math.floor(totalRenos / totalGifts);
+}
+    
+
+  const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
+
+
+
+console.log(distributeGifts(packOfGifts, reindeers)); // 2
